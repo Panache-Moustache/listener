62df3e9386b32cf18e1b5fa4490cae0aae5c1973
Greg <greg@greg.net.au>
2020-02-05 09:47:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,39 +5,43 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 // Initialize the client
 $client = new \Github\Client();
-$client2 = new \Github\Client();
 
-
-// Login to the Github API'
+// Login to the Github API. We need to specify our Username & API key here.
 $client->authenticate('', '', Github\Client::AUTH_HTTP_PASSWORD);
-$client2->authenticate('', '', Github\Client::AUTH_HTTP_PASSWORD);
+
 // Let's store the incoming webhook data into $data
 $data = file_get_contents('php://input');
 
 // Let's decode the json-encoded input
 $json_data = json_decode($data, true);
-#$repo_name = $json_data['repository']['name'];
-$repo_name = "test1234";
+$repo_name = $json_data['repository']['name'];
 
-
-// Here we need to protect the full_name branch that we have been notified about
+// Here we need to protect the master branch of the repository that we have been notified about
 // Post something to github API here
+
+// Connect to the Test API
+$client->api('repo')->protection()->configure();
+
+// Set the protection variables
 $params = [
-    'required_status_checks' => true,
+    'required_status_checks' => [
+        'strict' => true,
+        'contexts' => [
+        'continuous-integration/jenkins',
+        ],
+],
     'required_pull_request_reviews' => [
         'include_admins' => true,
     ],
     'enforce_admins' => true,
-    'restrictions' => true,
+    'restrictions' => null,
 ];
 
-$client->api('repo')->protection()->configure();
-$client->api('repo')->protection()->update('Panache-Moustache', $repo_name, 'master', $params);
-
+// Apply the protections
+$protection = $client->api('repo')->protection()->update('Panache-Moustache', $repo_name, 'master', $params);
 
 // Here we need to create an issue in the branch we have been notified about, and add tag ourselves!
 
-$client2->api('issue')->create('Panache-Moustache', 'test1234', array('title' => 'New Security Protections added to branch', 'body' => '@gregnetau the following protections have been added to '.$repo_name.' - Required Status Checks, Required Pull Request Reviews (including admins), Enforcing Admins and Restrictions.'));
+$client->api('issue')->create('Panache-Moustache', $repo_name, array('title' => 'New Security Protections added to branch', 'body' => '@gregnetau the following protections have been added to '.$repo_name.'.<br/>- Require Pull request reviews by admins.<br/>- Require jenkins CI status checks to pass before merging.<br/>- Including Admins on these restrictions.'));
 
 ?>
-
